c0a8320e73a9f83c159f84481aa2d6591a6f9722
Benjamin Hautier <benjamin.hautier@etudiant.univ-reims.fr>
2021-03-18 11:28:29 +0100

    creation de la terre

--- js/solar-system.js
+++ js/solar-system.js
@@ -2,21 +2,47 @@ var scene = new THREE.Scene();
 var aspect = window.innerWidth / window.innerHeight;
 var camera = new THREE.PerspectiveCamera( 75, aspect, 0.1, 1000 );
 var renderer = new THREE.WebGLRenderer();
-
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshNormalMaterial();
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-camera.position.z = 5;
+//ambient :
+var light = new THREE.AmbientLight( 0xb4e7f2 );
+scene.add( light );
+
+//directionnal light :
+var light = new THREE.DirectionalLight( 0xdddddd, 0.8 );
+    light.position.set( 20, 20, 0 );
+    scene.add( light );
+
+//star :
+var geometry = new THREE.SphereGeometry(10, 50, 50)
+
+    //textures :
+var loader = new THREE.TextureLoader();
+var texture = loader.load("./assets/8k_earth_daymap.jpg");
+var normal = loader.load("./assets/8k_earth_normal_map.tif");
+var specular = loader.load("./assets/8k_earth_specular_map.tif");
+
+var material = new THREE.MeshPhongMaterial( {
+    color: 0xaaaaaa,
+    specular: 0x333333,
+    shininess: 15,
+    map: texture,
+    specularMap: specular,
+    normalMap: normal
+});
+
+var star = new THREE.Mesh(geometry, material);
+scene.add(star);
+camera.position.z = 50;
+
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 var render = function () {
   requestAnimationFrame( render );
-  cube.rotation.x += 0.1;
-  cube.rotation.y += 0.1;
-  
+  //cube.rotation.x += 0.1;
+  //cube.rotation.y += 0.1;
+  controls.update();
   renderer.render( scene, camera );
 };
 
